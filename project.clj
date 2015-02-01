359c7118d1b5ce3bbafa0df540f0875f2b8131b5
Russell Whitaker <rwhitaker@ulive.com>
2015-01-31 23:18:08 -0800

    Added license info and other metadata to project

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject uap-clj "0.1.0-SNAPSHOT"
-  :description "Clojure language implementation of ua-parser"
+  :description "Clojure language implementation of ua-parser with Hive UDF wrappers"
+  :url "https://github.com/russellwhitaker/uap-clj"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [circleci/clj-yaml "0.5.3"]
                  [org.apache.hive/hive-exec "0.12.0"]
@@ -13,4 +16,4 @@
   :git-dependencies [["https://github.com/ua-parser/uap-core.git"]]
   :resource-paths [".lein-git-deps/uap-core"]
   :main uap-clj.core
-  :aot [uap-clj.core uap-clj.udf.browser uap-clj.udf.os])
+  :aot :all)
