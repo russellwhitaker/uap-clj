39350bbbfa43b9800747dbe9aed0fc74b5c69b7b
Russell Whitaker <rwhitaker@ulive.com>
2015-11-02 12:13:13 -0800

    Refactored test suite in advance of uap-clj.core refactoring

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uap-clj "1.0.3"
+(defproject uap-clj "1.1.0"
   :description "Clojure language implementation of ua-parser"
   :url "https://github.com/russellwhitaker/uap-clj"
   :license {:name "The MIT License (MIT)"
@@ -7,14 +7,16 @@
         :url "https://github.com/russellwhitaker/uap-clj"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [circleci/clj-yaml "0.5.3"]]
-  :jvm-opts ["-Xss2m"]
   :profiles {:dev
               {:dependencies [[speclj "3.3.1"]
-                              [lein-git-deps "0.0.2"]]}}
+                              [lein-git-deps "0.0.2"]]
+               :test-paths ["spec"]
+               :jvm-opts ["-Xss4m"]}
+             :uberjar {:aot :all
+                       :uberjar-exclusions
+                         [#"tests|test_resources|docs|\.md|LICENSE|META-INF"]}}
   :plugins [[lein-git-deps "0.0.2"]
             [speclj "3.3.1"]]
-  :test-paths ["spec"]
   :git-dependencies [["https://github.com/ua-parser/uap-core.git"]]
   :resource-paths [".lein-git-deps/uap-core"]
-  :main uap-clj.core
-  :aot :all)
+  :main uap-clj.core)
