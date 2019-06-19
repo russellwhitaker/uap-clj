517ab41241f4acc1a02ed4a3a82e824946e077ca
Russell Whitaker <russell.whitaker@gmail.com>
2019-06-19 15:53:37 -0700

    Pared down cruft

--- project.clj
+++ project.clj
@@ -6,22 +6,23 @@
   :scm {:name "git"
         :url "https://github.com/russellwhitaker/uap-clj"}
   :dependencies [[org.clojure/clojure      "1.10.1"]
-                 [russellwhitaker/immuconf "0.3.0"]
+                 [russellwhitaker/immuconf "0.3.0"
+                   :exclusions [org.clojure/clojurescript
+                                com.taoensso/timbre]]
                  [clj-commons/clj-yaml     "0.7.0"]]
+  :jar-exclusions [#"dev_resources|^test$|test_resources|tests|docs|\.md|LICENSE|package.json"]
   :profiles {:dev
               {:dependencies [[criterium "0.4.5"]
                               [speclj    "3.3.2"]]
                :jvm-opts ["-Xss256M"]
                :test-paths ["spec"]}
-             :uberjar {:aot :all
-                       :uberjar-exclusions
-                         [#"dev_resources|^test$|test_resources|docs|\.md|LICENSE"]}}
+             :uberjar {:uberjar-exclusions
+                        [#"dev_resources|^test$|test_resources|tests|docs|\.md|LICENSE|package.json"]}}
   :plugins [[lein-git-deps "0.0.2"]
             [lein-ancient  "0.6.15"]
             [speclj        "3.3.2"]]
   :git-dependencies [["https://github.com/russellwhitaker/uap-core.git" "rew/fix_test_ua_yaml_tab"]]
   :resource-paths [".lein-git-deps/uap-core"]
-  :main uap-clj.core
-  :aot :all
+  :main ^:skip-aot uap-clj.core
   :aliases {"test"  ["do" ["clean"] ["spec" "--reporter=d"]]
             "build" ["do" ["clean"] ["uberjar"]]})
