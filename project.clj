ffbe6d7129b8b3fb3fd4907937c19cc5d9ffc17b
Quest Yarbrough <quest@yarbrough.net>
2022-01-31 20:44:25 -0800

    Fix deps.edn compilation by reverting to older immuconf version (#3)
    CLJS support of forked child version isn't needed for this repo,
    but it does break deps.edn compilation with reader literal error
    see https://github.com/levand/immuconf/issues/10
    
    Co-authored-by: Quest Yarbrough (*) <qyarbrough@GR-KCX30HW4Vmbp.local>

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject uap-clj "1.3.6"
+(defproject uap-clj "1.3.7"
   :description "Clojure language implementation of ua-parser"
   :url "https://github.com/russellwhitaker/uap-clj"
   :license {:name "The MIT License (MIT)"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :scm {:name "git"
         :url "https://github.com/russellwhitaker/uap-clj"}
-  :dependencies [[org.clojure/clojure      "1.10.1"]
-                 [russellwhitaker/immuconf "0.3.0"
-                   :exclusions [org.clojure/clojurescript
-                                com.taoensso/timbre]]
+  :dependencies [[org.clojure/clojure      "1.10.3"]
+                 [levand/immuconf          "0.1.0"]
                  [clj-commons/clj-yaml     "0.7.0"]]
   :jar-exclusions [#"dev_resources|^test$|test_resources|tests|docs|\.md|LICENSE|package.json"]
   :profiles {:dev
